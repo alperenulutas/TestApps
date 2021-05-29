fab5534470cd72d909661ea6dc107195da8c04da
alperenulutas <85023324+alperenulutas@users.noreply.github.com>
2021-05-29 16:38:05 +0300

    Add files via upload

---
+++ TestApplication/TestApplication/Animal.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestApplication
+{
+    public class Animal
+    {
+        public void sound(string animalType)
+        {
+            if (animalFamily(animalType) == "Vahşi")
+            {
+                Console.WriteLine("Warning !! It can be dangerous !!");
+            }
+
+            if (animalType == "Dog")
+            {
+                Console.WriteLine("Bark");
+            }
+            else if (animalType == "Cat")
+            {
+                Console.WriteLine("Miav");
+            }
+            else if (animalType == "Bird")
+            {
+                Console.WriteLine("Cik");
+            }
+            else if (animalType == "Lion")
+            {
+                Console.WriteLine("Hırr");
+            }
+            else
+            {
+                Console.WriteLine("undefined");
+            }               
+        }
+        public int numberOfLegs(string animalType)
+        {
+            int leg = 0;
+            if (animalType == "Dog")
+            {
+                leg = 4;
+            }
+            else if (animalType == "Cat")
+            {
+                leg = 4;
+            }
+            else if (animalType == "Bird")
+            {
+                leg = 2;
+            }
+            else
+            {
+                leg = -1;
+            }
+            return leg;
+        }
+        private string animalFamily(String animalType)
+        {
+            string family;
+
+            if (animalType == "Dog")
+            {
+                family = "Evcil";
+            }
+            else if (animalType == "Cat")
+            {
+                family = "Evcil";
+            }
+            else if (animalType == "Lion")
+            {
+                family = "Vahşi";
+            }
+            else
+            {
+                family = "undefined";
+            }
+            return family;
+        }
+    }
+}
---
+++ TestApplication/TestApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal ani = new Animal();          
+            
+            ani.sound("Dog");
+            ani.sound("Lion");
+            ani.sound("Rabbit");
+
+        }
+        
+    }
+}
